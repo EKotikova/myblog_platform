c2b026828ba07da10e4ac001589f5e65768b1497
helen <kotik04@gmail.com>
2018-07-26 07:13:28 +0300

    Create functions  'edit' and  'delete'  for post

--- components/js/edit.js
+++ components/js/edit.js
@@ -6,9 +6,16 @@
 var edit=function(){
     var  texted=document.getElementById('post_full');
     alert(texted);
-    var areatxt=document.getElementById('');
+    var areatxt=document.getElementById('corect');
+    alert(areatxt);
     areatxt.innerText=texted.innerText;
         $(function(){
             $('div.' + $(this).attr("rel")).fadeIn(500);
+
+            $('a.close').click(function () {
+                $(this).parent().fadeOut(100);
+                //  $('#overlay_new').remove('#overlay_new');
+                return false;
+            });
         });
 }
---
+++ model/edite_form.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lena
+ * Date: 26.07.2018
+ * Time: 6:49
+ */?>
+
+
+
+<div id="edit" class="popup edit">
+    <a class='close' href='#'>Close</a>
+    <div id="form_corect">
+        <textarea id="corect"></textarea>
+    </div>
+</div>
