fb8a103f765c3e3bbb5c2bc5e2272731c311faeb
helen <kotik04@gmail.com>
2018-07-27 09:20:09 +0300

    Create page registration for blog_platform

---
+++ model/userdata.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lena
+ * Date: 27.07.2018
+ * Time: 7:00
+ */
+include "components/connect_db.php";
+if(isset($_POST['user'])&& isset($_POST['email'])&& isset($_POST['password'])&& isset($_POST['confpass'])){
+    $user=$_POST['user'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $confpass=$_POST['confpass'];
+
+
+    $db=mysqli_connect($host,$user,$password,$database) or
+    die("Error connect db :".mysqli_error($db) );
+
+    $query="Insert into users (nameusers,email,password)VALUES ('".$user."','".$email."','".$password."')";
+
+    if($password==$confpass){
+        $res=mysqli_query($db,$query) or die("error insert:".mysqli_error($db));
+        if(isset($res)){
+            header('Location:http://myblog:81/authorization.php');
+        }
+    }
+    else{
+        echo "Please check password";
+    }
+
+}
