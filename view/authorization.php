1cd5c60594698a2602f3cfffa0250c6583de696e
helen <kotik04@gmail.com>
2018-07-27 16:27:49 +0300

    Create page authorization for blog_platform

--- view/authorization.php
+++ view/authorization.php
@@ -6,17 +6,68 @@
  * Time: 14:36
  */?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-<form id="Sign in" method="post" action="/index.php?authorizin=true">
-    <a href="authorization.php">Don't have an acount?Join now</a>
-    <div class="form-group">
-        <label for="InputEmail">Email address</label>
-        <input name="email" type="email" class="form-control" id="InputEmail" required  placeholder="Email address">
-    </div>
-    <div class="form-group">
-        <label for="Password1">Password</label>
-        <input name="password" type="password" class="form-control" id="Password1" required placeholder="Password">
-    </div>
-    <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
+<head>
+    <meta charset="UTF-8">
+    <title>My blogs_platforma</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css"
+          integrity="2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src=""></script>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<div>
+    <header>
+    </header>
+
+    <form id="Sign in" method="post" action="/index.php?authorizin=true">
+        <a href="authorization.php">Don't have an acount?Join now</a>
+        <div class="form-group">
+            <label for="InputEmail">Email address</label>
+            <input name="email" type="email" class="form-control" id="InputEmail" required  placeholder="Email address">
+        </div>
+        <div class="form-group">
+            <label for="Password1">Password</label>
+            <input name="password" type="password" class="form-control" id="Password1" required placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
+
+
+</div>
+
+
+<footer>
+    Copyright Kotikova  Elena 2018
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"
+        integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js"
+        integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js"
+        integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
