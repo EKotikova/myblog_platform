0dd857829c06f4a7eeb9de7b9e2e106d357e2f94
helen <kotik04@gmail.com>
2018-08-01 08:36:27 +0300

    Create page authorization for blog_platform

--- view/authorization.php
+++ view/authorization.php
@@ -4,7 +4,25 @@
  * User: Lena
  * Date: 23.07.2018
  * Time: 14:36
- */?>
+ */
+
+if(isset($_GET['name'])){
+    include "components/connect_db.php";
+    $db=mysqli_connect($host,$user,$password,$database) or
+    die("Error connect db :".mysqli_error($db) );
+
+    $queryrole="Select id_roles from users where nameusers='".$_GET['name']."'";
+    $resrole=mysqli_query($db,$queryrole)or die('select role:'.mysqli_error($db));
+    if(isset($resrole)){
+        $row=mysqli_fetch_array($resrole);
+        if($row['id_roles']==2){
+            echo "<script>document.location.replace('http://myblog:81/index.php?name=".$_GET['name']."')</script>";
+        }
+    }
+
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,21 +42,18 @@
 <div>
     <header>
     </header>
-
-    <form id="Sign in" method="post" action="/index.php?authorizin=true">
-        <a href="authorization.php">Don't have an acount?Join now</a>
-        <div class="form-group">
-            <label for="InputEmail">Email address</label>
-            <input name="email" type="email" class="form-control" id="InputEmail" required  placeholder="Email address">
-        </div>
-        <div class="form-group">
+    <center>
+    <a href="/index.php?reg=true&name=<?echo $_GET['name']?>">Don't have an acount?Join now</a>
+    <form id="Signin" method="post" action="/index.php?authorizin=true">
+            <label for="InputEmail1">Email address</label>
+            <input name="email" type="email" class="form-control" id="InputEmail1" required  placeholder="Email address">
+             <span class="help-text"></span>
             <label for="Password1">Password</label>
             <input name="password" type="password" class="form-control" id="Password1" required placeholder="Password">
-        </div>
+             <span class="help-text"></span>
         <button type="submit" class="btn btn-primary">Sign in</button>
     </form>
-
-
+    </center>
 </div>
 
 
