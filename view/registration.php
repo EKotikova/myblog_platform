b7644b82b36c6d128c5f8be194f08b6bdb048463
helen <kotik04@gmail.com>
2018-08-01 08:34:31 +0300

    Create page registration for blog_platform

--- view/registration.php
+++ view/registration.php
@@ -5,6 +5,22 @@
  * Date: 23.07.2018
  * Time: 14:36
  */
+if(isset($_GET['name'])){
+    include "components/connect_db.php";
+    $db=mysqli_connect($host,$user,$password,$database) or
+    die("Error connect db :".mysqli_error($db) );
+
+    $queryrole="Select id_roles from users where nameusers='".$_GET['name']."'";
+    $resrole=mysqli_query($db,$queryrole)or die('select role:'.mysqli_error($db));
+    if(isset($resrole)){
+        $row=mysqli_fetch_array($resrole);
+        if($row['id_roles']==2){
+            echo "<script>document.location.replace('http://myblog:81/index.php?name=".$_GET['name']."')</script>";
+        }
+    }
+
+}
+
 
 ?>
 
@@ -13,78 +29,70 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>My blogs_platforma</title>
+    <title>Registration</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css"
           integrity="2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script type="text/javascript" src=""></script>
     <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
 <div>
     <header>
     </header>
 
-    <form id="Signup" method="post" action="/index.php?regin=true">
-        <a href="authorization.php">Already registered?Sign in</a>
-        <div class="form-group">
+    <div id="main">
+        <center>
+        <a href="/index.php?user=true&name=<?echo $_GET['name']?>">Already registered?Sign in</a>
+    <form id="Signup" method="post" action="/index.php?regin=true&role=User">
             <label for="username">Username</label>
-            <input  name="user" type="text" class="form-control" id="username" required oninput="validateName(this)"  placeholder="Username">
-        </div>
-        <div class="form-group">
-            <label for="InputEmail1">Email address</label>
-            <input name="email" type="email" class="form-control" id="InputEmail1" oninput="validateEmail(this)"  required placeholder="Email address">
-        </div>
-        <div class="form-group">
+            <input  name="user" type="text" class=" form-control" id="username"  oninput="validateName(this)" required pattern="^[a-zA-Z][a-zA-Z0-9-_/.]{1,16}$" placeholder="Username">
+             <span class="help-text"></span>
+
+             <label for="InputEmail1">Email address</label>
+            <input  name="email" type="email" class="form-control" id="InputEmail1"  required placeholder="Email address">
+             <span class="help-text"></span>
+
             <label for="Password1">Password</label>
-            <input name="password" type="password" class="form-control" id="Password1" oninput="validatePassword(this)" required placeholder="Password">
-        </div>
-        <div class="form-group">
+            <input  name="password" type="password" class="form-control" id="Password1"  required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder="Password">
+             <span class="help-text"></span>
+
             <label for="ConfirmPassword1"> Confirm Password</label>
-            <input name="confpass" type="password" class="form-control" id="ConfirmPassword1" oninput="validateConfPassword(this)" required placeholder=" Confirm Password">
-        </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
+            <input  name="confpass" type="password" class="form-control" id="ConfirmPassword1" oninput="validateConfPassword(this)" required placeholder=" Confirm Password">
+             <span class="help-text"></span>
+            <button id="in" type="submit" class="btn btn-primary">Sign in</button>
     </form>
+    </center>
+        </div>
 
     <script>
-        function validateName(input){
-            if(!input.value.match('^[a-zA-Z][a-zA-Z0-9-_/.]{1,16}$')){
-                input.setCustomValidity("The user must start with a capital letter, must contain only letters and numbers in Latin. " +
-                    "The maximum length is 16 characters.");
+            function validateName(input) {
+                if (input.value.length > 16) {
+                    (input).setCustomValidity("The user must start with a capital letter, must contain only letters and numbers in Latin.The maximum length is 16 characters.");
+                }
+                else {
+                    (input).setCustomValidity("");
+                }
             }
-            else {
-                input.setCustomValidity("");
-            }
-        }
 
-        function validateEmail(input){
-            if(!input.value.match('/^([a-z0-9_/.-])+@[a-z0-9-]+/.([a-z]{2,4}/.)?[a-z]{2,4}$/i')){
-                input.setCustomValidity("not valid this email address");
+            function validatePassword(input) {
+                if (input.value.length<8) {
+                    (input).setCustomValidity("The password field must be at least 8 characters. Must contain lowercase and uppercase letters and at least one digit.");
+                }
+                else {
+                    (input).setCustomValidity("");
+                }
             }
-            else {
-                input.setCustomValidity("");
-            }
-        }
 
-        function validatePassword(input){
-            if(!input.value.match('(?=^.{8,}$)((?=.*/d)|(?=.*/W+))(?![./n])(?=.*[A-Z])(?=.*[a-z]).*$')){
-                input.setCustomValidity("The password field must be at least 8 characters. Must contain lowercase and uppercase letters and at least one digit.");
+            function validateConfPassword(input) {
+                if (document.getElementById('Password1').value != document.getElementById('ConfirmPassword1').value) {
+                    (input).setCustomValidity("This password is't valid");
+                }
+                else {
+                    (input).setCustomValidity("");
+                }
             }
-            else {
-                input.setCustomValidity("");
-            }
-        }
-
-        function validateConfPassword(input){
-            if(document.getElementById('Password1').value!=document.getElementById('ConfirmPassword1').value){
-                input.setCustomValidity("This password is't valid");
-            }
-            else {
-                input.setCustomValidity("");
-            }
-        }
 
     </script>
 
@@ -92,9 +100,7 @@
 </div>
 
 
-<footer>
-    Copyright Kotikova  Elena 2018
-</footer>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"
         integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY"
@@ -106,6 +112,9 @@
         integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
 
 </body>
+<footer>
+    Copyright Kotikova  Elena 2018
+</footer>
 </html>
 
 
